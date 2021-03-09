7d285c9f13233a55197bd40a0539d559a98e5ad2
Nameless <truemedian@gmail.com>
2021-03-08 22:47:29 -0600

    fix docs, update README

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 ---A combination of extensions to the Lua standard libraries and other useful utilities for libraries I use.
 ---@module extensions
+
 -- This initialization module is designed for use in the Luvit environment.
 -- It *will* not work elsewhere, however you can still copy the individual
 -- extensions from the `libs` directory and they should work.
@@ -14,27 +15,27 @@ extensions.table = require('table.lua')
 extensions.coroutine = require('coroutine.lua')
 
 local autoload = {
-    math = extensions.math,
-    string = extensions.string,
-    table = extensions.table,
-    coroutine = extensions.coroutine,
+	math = extensions.math,
+	string = extensions.string,
+	table = extensions.table,
+	coroutine = extensions.coroutine,
 }
 
 ---Loads the standard library extensions into the global environment. This will **not** overwrite any user-defined functions.
 ---@usage local extensions = require 'extensions' ()
 function extensions.load()
-    for name, ext in pairs(autoload) do
-        for k, v in pairs(ext) do
-            if _G[name][k] == nil then
-                _G[name][k] = v
-            end
-        end
-    end
+	for name, ext in pairs(autoload) do
+		for k, v in pairs(ext) do
+			if _G[name][k] == nil then
+				_G[name][k] = v
+			end
+		end
+	end
 end
 
 function extensions.meta:__call()
-    self.load()
-    return self
+	self.load()
+	return self
 end
 
 setmetatable(extensions, extensions.meta)
