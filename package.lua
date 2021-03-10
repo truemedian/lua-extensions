d3aad6ea4bdca74e78488cde3d3ea25e6c928b57
Nameless <truemedian@gmail.com>
2021-03-10 07:25:10 -0600

    package for lit

---
+++ package.lua
@@ -0,0 +1,11 @@
+return {
+    name = 'truemedian/extensions',
+    version = '1.0.0',
+    description = 'A combination of extensions to the Lua standard libraries and other useful utilities for libraries I use.',
+    tags = {'lua', 'luvit', 'extension', 'stdlib'},
+    license = 'MIT',
+    author = {name = 'Nameless'},
+    homepage = 'https://github.com/truemedian/lua-extensions',
+    dependencies = {},
+    files = {'**.lua'},
+}
