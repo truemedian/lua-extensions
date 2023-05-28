66e82fdbf2bbb642b6ec551b4e9839379cc4cd26
Nameless <truemedian@gmail.com>
2023-05-28 22:32:49 +0200

    fix plain endswith
    closes #18

--- libs/string.lua
+++ libs/string.lua
@@ -26,7 +26,7 @@ end
 ---@return boolean
 function ext_string.endswith(str, pattern, plain)
 	if plain then
-		return select(2, string.sub(str, - #pattern)) == #str
+		return string.sub(str, -#pattern) == pattern
 	else
 		if string.sub(pattern, -1) ~= '$' then
 			pattern = pattern .. '$'
